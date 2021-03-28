8058bf10ec394fb13744c0c80b38b65a0213a3d7
Gabriel Oliveira Brito <gabriel.oliveira.brito@outlook.com>
2021-03-28 16:37:26 -0300

    Update Constants.java

--- app/src/main/java/com/gobrito/youtubevideoinfo/Constants.java
+++ app/src/main/java/com/gobrito/youtubevideoinfo/Constants.java
@@ -1,7 +1,7 @@
 package com.gobrito.youtubevideoinfo;
 
 public class Constants {
-    public static final String GoogleApiKey = "AIzaSyBGqxNoXBYPfzrZzYyJ3CMeoMQws7tOXcE";
+    public static final String GoogleApiKey = "";
 
     public static final String PREF_ID = "youtube_video_info";
 }
